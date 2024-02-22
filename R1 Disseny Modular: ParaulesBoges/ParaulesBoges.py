476d38e4b3137df2b5db4fdb79ba544e25cab806
Paraules Boges <aitor.palma.7e5@itb.cat>
2024-02-22 12:51:57 +0100

    update

--- R1 Disseny Modular: ParaulesBoges/ParaulesBoges.py
+++ R1 Disseny Modular: ParaulesBoges/ParaulesBoges.py
@@ -1,18 +1,20 @@
 ### Aitor Palma ### ###---### ### Iker Jimenez ###
 ### 22/02/2024 ### ###---### ### Paraules Boges ###
 
+import re
 
 def paraulesBoges(frase):
-    paraules = frase.split()
+    paraules = re.findall(r'\b\w+\b', frase)
     fraseBoga = ""
     for paraula in paraules:
-        if len(paraula) > 3:
-            paraula = paraula[0] + paraula[1:-1][::-1] + paraula[-1]
-        fraseBoga += paraula + " "
+        if len(paraula) > 2:
+            fraseBoga += paraula[0] + paraula[1:-1][::-1] + paraula[-1] + " "
+        else:
+            fraseBoga += paraula + " "
     return fraseBoga
 
-frase = input("Frase: ")
+frase = input("Introdueix una frase: ")
 
-print("Sortida:", paraulesBoges(frase))
+print(paraulesBoges(frase))
 
 
