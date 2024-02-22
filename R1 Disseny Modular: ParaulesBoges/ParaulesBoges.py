492ca7cd5539d19b63032c925aa102a4fd3ff923
Paraules Boges <aitor.palma.7e5@itb.cat>
2024-02-22 13:02:00 +0100

    update

--- R1 Disseny Modular: ParaulesBoges/ParaulesBoges.py
+++ R1 Disseny Modular: ParaulesBoges/ParaulesBoges.py
@@ -5,15 +5,13 @@ import re
 
 def paraulesBoges(frase):
     paraules = re.findall(r'\b\w+\b', frase)
-    fraseBoga = ""
     for paraula in paraules:
         if len(paraula) > 2:
-            fraseBoga += paraula[0] + paraula[1:-1][::-1] + paraula[-1] + " "
-        else:
-            fraseBoga += paraula + " "
-    return fraseBoga
+            paraulaBoga = paraula[0] + paraula[1:-1][::-1] + paraula[-1]
+            frase = frase.replace(paraula, paraulaBoga)
+    return frase
 
-frase = input("Posa una frase: ")
+frase = input("Introdueix una frase: ")
 
 print(paraulesBoges(frase))
 
